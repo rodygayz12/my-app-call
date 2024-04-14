a188422e1b76727e46177f48e7ba3d8d92ce2c51
rodygayz12 <161971854+rodygayz12@users.noreply.github.com>
2024-04-14 21:03:22 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const { Server } = require('socket.io');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    socket.roomId = roomId;
+  });
+
+  socket.on('offer', (offer, roomId) => {
+    socket.to(roomId).emit('offer', offer);
+  });
+
+  socket.on('answer', (answer, roomId) => {
+    socket.to(roomId).emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate, roomId) => {
+    socket.to(roomId).emit('ice-candidate', candidate);
+  });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-
-  socket.on('call', () => {
-    io.emit('incomingCall');
-  });
-
-  socket.on('answer', () => {
-    io.emit('startCall');
-  });
-
-  socket.on('endCall', () => {
-    io.emit('callEnded');
+    socket.to(socket.roomId).emit('user-disconnected');
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
